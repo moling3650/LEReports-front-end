75b19486ebdf32af37944c291611a1e52481d219
moling <365024424@qq.com>
2018-03-22 13:56:39 +0800

    导入ElementUI，添加reportEditor组件

---
+++ src/components/reportEditor.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="reportEditor">
+    <el-row :gutter="20">
+      <el-col class="cell" :span="6">
+        <el-select :value="reportCode" clearable filterable placeholder="请选择报表" @change="handleReportChange">
+          <el-option v-for="item in reports" :key="item.value" :label="item.label" :value="item.value"/>
+        </el-select>
+      </el-col>
+      <el-col class="cell" :span="18">
+        <el-button @click="openReportForm">{{ reportCode ? '编辑' : '新建' }}报表</el-button>
+        <el-button @click="deleteReport" type="danger">删除报表</el-button>
+        <el-button @click="openReportURL" type="primary">打开报表</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'reportEditor',
+  data () {
+    return {
+      reportCode: '',
+      reports: []
+    }
+  },
+  methods: {
+    handleReportChange (reportCode) {
+    },
+    openReportForm () {
+    },
+    openReportURL () {
+    },
+    deleteReport () {
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/pages/reportBuilder.vue
+++ src/pages/reportBuilder.vue
@@ -1,13 +1,18 @@
 <template>
-  <div id="reportBuilder">reportBuilder</div>
+  <div id="reportBuilder">
+    <report-editor></report-editor>
+  </div>
 </template>
 
 <script>
+import reportEditor from '@/components/reportEditor'
 export default {
   name: 'reportBuilder',
-  data () {
-    return {
-    }
+  components: {
+    reportEditor
+  },
+  created () {
+    window.document.title = '报表编辑器'
   }
 }
 </script>
