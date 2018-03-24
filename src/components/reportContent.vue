a71d2c4f1027615813ed554b207821a4dc23469b
moling <365024424@qq.com>
2018-03-24 11:06:53 +0800

    添加reportContent组件

---
+++ src/components/reportContent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="reportContent">
+    <el-table v-loading="loading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(0, 0, 0, 0.8)"
+      :data="tableData"
+      border stripe
+      size="mini"
+      style="width: 100%"
+      :header-cell-style="{backgroundColor: '#409eff', color: '#fff'}"
+      tooltip-effect="dark"
+    >
+      <el-table-column prop="prop" label="字段属性" align="center"/>
+      <el-table-column prop="label" label="字段标签" align="center"/>
+      <el-table-column prop="width" label="字段宽度" :formatter="fmtWidth" align="center"/>
+      <el-table-column prop="idx" label="字段排序" align="center"/>
+      <el-table-column prop="align" label="字段对齐" align="center"/>
+      <el-table-column prop="state" label="是否可见" :formatter="fmtBoolean" align="center"/>
+      <el-table-column prop="is_check" label="是否可查询" :formatter="fmtBoolean" align="center"/>
+      <el-table-column prop="control_code" label="控件" :formatter="fmtControl" align="center"/>
+      <el-table-column prop="control_span" label="控件宽度" align="center"/>
+      <el-table-column prop="required" label="是否必填" :formatter="fmtBoolean" align="center"/>
+      <el-table-column prop="options_api" label="选择器API" align="center" show-overflow-tooltip/>
+      <el-table-column label="操作" align="center">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleRowEdit(scope.$index, scope.row)">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'reportContent',
+  computed: mapGetters({
+    tableData: 'fieldsData',
+    loading: 'loadingFields'
+  }),
+  methods: {
+    fmtBoolean (row, column, cellValue) {
+      return cellValue ? '是' : '否'
+    },
+    fmtControl (row, column, cellValue) {
+      const control = this.$store.getters.queryControlOptions.find(c => c.value === cellValue)
+      return control ? control.label : '未知控件'
+    },
+    fmtWidth (row, column, cellValue) {
+      cellValue = cellValue !== undefined ? cellValue : row
+      return cellValue ? `${cellValue}px` : '自适应'
+    },
+    handleRowEdit () {
+
+    }
+  },
+  created () {
+    this.$store.dispatch('getQueryControls')
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#reportContent {
+  margin-top: 5px;
+}
+</style>
--- src/pages/reportBuilder.vue
+++ src/pages/reportBuilder.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="reportBuilder">
     <report-editor></report-editor>
+    <report-content></report-content>
   </div>
 </template>
 
 <script>
 import reportEditor from '@/components/reportEditor'
+import reportContent from '@/components/reportContent'
 export default {
   name: 'reportBuilder',
   components: {
-    reportEditor
+    reportEditor,
+    reportContent
   },
   created () {
     window.document.title = '报表编辑器'
