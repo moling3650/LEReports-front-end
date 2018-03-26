a1f7cc3681f7f0b45717f55742286a1c4a897656
moling <365024424@qq.com>
2018-03-26 18:02:05 +0800

    添加mInput表单组件

---
+++ src/components/formItems/mInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-input v-model="rawValue" clearable :placeholder="`请输入${label}`"/>
+</template>
+
+<script>
+export default {
+  name: 'mInput',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      rawValue: ''
+    }
+  },
+  computed: {
+    value () {
+      return this.rawValue.trim()
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
