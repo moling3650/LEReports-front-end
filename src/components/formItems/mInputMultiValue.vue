3496d62456db4b4d489f651e741d8e03a52e5cc5
moling <365024424@qq.com>
2018-03-26 18:03:20 +0800

    添加mInputMultiValue表单组件

---
+++ src/components/formItems/mInputMultiValue.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-input v-model="rawValue" clearable :placeholder="`请输入一个或多个${label}`"/>
+</template>
+
+<script>
+export default {
+  name: 'mInputMultiValue',
+  props: {
+    label: {
+      type: String,
+      default: '数值'
+    }
+  },
+  data () {
+    return {
+      rawValue: ''
+    }
+  },
+  computed: {
+    value () {
+      return this.rawValue.trim() && this.rawValue.trim().split(/[\s,]+/)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
