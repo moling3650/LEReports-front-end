ba6e1d88e448fbdf5fc7ae6372fbb14c9aa8eec1
moling <365024424@qq.com>
2018-03-26 18:02:55 +0800

    添加mInputLike表单组件

---
+++ src/components/formItems/mInputLike.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-input v-model="rawValue" clearable :placeholder="`模糊查询${label}`"/>
+</template>
+
+<script>
+export default {
+  name: 'mInputLike',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      rawValue: ''
+    }
+  },
+  computed: {
+    value () {
+      return this.rawValue.trim()
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
