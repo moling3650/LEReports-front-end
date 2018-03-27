151f30bb64255f3592a9342346d54794fc66fa3a
moling <365024424@qq.com>
2018-03-27 13:51:02 +0800

    添加API

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,6 +4,11 @@ function fetchQueryControls () {
   return axios.get('/API/Handler.ashx?method=GetQueryControls').then(res => res.data)
 }
 
+function fetchRecords (params) {
+  return axios.post('/API/Handler.ashx?method=GetRecords', params).then(res => res.data)
+}
+
 export {
-  fetchQueryControls
+  fetchQueryControls,
+  fetchRecords
 }
