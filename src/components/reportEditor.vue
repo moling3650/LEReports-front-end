23ff52c9ae3b65b907d086261bdfb7d8bfedf7db
moling <365024424@qq.com>
2018-03-30 13:34:37 +0800

    更新reportEditor组件添加图表模块

--- src/components/reportEditor.vue
+++ src/components/reportEditor.vue
@@ -3,55 +3,85 @@
     <el-row :gutter="20">
       <el-col class="cell" :span="6">
         <el-select v-model="reportCode" clearable filterable placeholder="请选择报表">
-          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"/>
+          <el-option v-for="item in reportOptions" :key="item.value" :label="item.label" :value="item.value"/>
         </el-select>
       </el-col>
-      <el-col class="cell" :span="18">
-        <el-button @click="handleEdit">{{ reportCode ? '编辑' : '新建' }}报表</el-button>
-        <el-button @click="handleDelete" type="danger">删除报表</el-button>
-        <el-button @click="handleOpen" type="primary">打开报表</el-button>
+      <el-col class="cell" :span="6">
+        <el-button-group>
+          <el-button @click="handleEditReport" :type="reportCode ? 'primary' : 'success'">
+            {{ reportCode ? '编辑' : '新建' }}报表
+          </el-button>
+          <el-button @click="handleDeleteReport" type="danger">删除报表</el-button>
+          <el-button @click="handleOpen">打开报表</el-button>
+        </el-button-group>
+      </el-col>
+
+      <el-col v-show="reportCode" class="cell" :span="6">
+        <el-select v-model="chartId" clearable filterable placeholder="请选择图表">
+          <el-option v-for="item in chartOptions" :key="item.value" :label="item.label" :value="item.value"/>
+        </el-select>
+      </el-col>
+      <el-col v-show="reportCode" class="cell" :span="6">
+        <el-button-group>
+          <el-button @click="handleEditChart" :type="chartId ? 'primary' : 'success'">
+            {{ chartId ? '编辑' : '新建' }}图表
+          </el-button>
+          <el-button @click="handleDeleteChart" type="danger">删除图表</el-button>
+        </el-button-group>
       </el-col>
     </el-row>
-    <report-form :report-code="reportCode" ref="reportform" @update-report-code="setReportCode"/>
+    <report-form :report-code="reportCode" ref="reportForm" @update-report-code="setReportCode"/>
+    <chart-form :report-code="reportCode" :chart-id="chartId" ref="chartForm"/>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import reportForm from '@/components/reportForm'
+import chartForm from '@/components/Dialog/chartForm'
 
 export default {
   name: 'reportEditor',
   data () {
     return {
-      reportCode: ''
+      reportCode: '',
+      chartId: ''
     }
   },
   computed: mapGetters({
-    options: 'reportOptions'
+    reportOptions: 'reportOptions',
+    chartOptions: 'chartOptions'
   }),
   components: {
-    reportForm
+    reportForm,
+    chartForm
   },
   watch: {
     reportCode () {
-      this.$store.dispatch('getFieldsData', this.reportCode)
+      this.chartId = ''
+      if (this.reportCode) {
+        this.$store.dispatch('getFieldsData', this.reportCode)
+        this.$store.dispatch('getChartsByReport', this.reportCode)
+      } else {
+        this.$store.commit('setFieldsData', [])
+        this.$store.commit('setCharts', [])
+      }
     }
   },
   methods: {
     setReportCode (reportCode) {
       this.reportCode = reportCode
     },
-    handleEdit () {
-      this.$refs.reportform.open()
+    handleEditReport () {
+      this.$refs.reportForm.open()
     },
     handleOpen () {
       if (!this.reportCode) {
-        return void this.$message({ showClose: true, message: '请先选择报表' })
+        return void this.$message({ message: '请先选择报表', showClose: true, duration: 1500 })
       }
       window.open(`${location.origin}${location.pathname}#/ReportQuerier?reportCode=${this.reportCode}`)
     },
-    handleDelete () {
+    handleDeleteReport () {
       if (!this.reportCode) {
         return void this.$message({ showClose: true, message: '请先选择报表' })
       }
@@ -62,12 +92,30 @@ export default {
       })
         .then(() => this.$store.dispatch('deleteReport', this.reportCode))
         .then(() => this.$set(this, 'reportCode', ''))
-        .then(() => this.$message.success('删除成功!'))
-        .catch(() => this.$message.info('已取消删除'))
+        .then(() => this.$message.success({ message: '删除成功!', showClose: true, duration: 1500 }))
+        .catch(() => this.$message.info({ message: '已取消删除', showClose: true, duration: 1500 }))
+    },
+    handleEditChart () {
+      this.$refs.chartForm.open()
+    },
+    handleDeleteChart () {
+      if (!this.chartId) {
+        return void this.$message({ message: '请先选择报表', showClose: true, duration: 1500 })
+      }
+      this.$confirm('此操作将永久删除该图表, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => this.$store.dispatch('deleteChart', this.chartId))
+        .then(() => this.$set(this, 'chartId', ''))
+        .then(() => this.$message.success({ message: '删除成功!', showClose: true, duration: 1500 }))
+        .catch(() => this.$message.info({ message: '已取消删除', showClose: true, duration: 1500 }))
     }
   },
   created () {
     this.$store.dispatch('getAllReports')
+    this.$store.dispatch('getChartTypes')
   }
 }
 </script>
