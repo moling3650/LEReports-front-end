9476a3a05efe27887e055f3b8dfdd22bb1c50c90
moling <365024424@qq.com>
2018-04-01 00:44:53 +0800

    添加chartProp组件

---
+++ src/components/Dialog/chartProp.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-dialog title="编辑图表" :visible.sync="visible">
+    <el-form :model="form" :rules="rules" ref="form">
+      <el-form-item label="类别" :label-width="formLabelWidth" prop="catgory">
+        <el-select v-model="form.catgory" placeholder="请选择类别">
+          <el-option
+            v-for="item in fieldOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="数据" :label-width="formLabelWidth" prop="value">
+        <el-select v-model="form.value" multiple placeholder="请选择数据">
+          <el-option
+            v-for="item in fieldOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取 消</el-button>
+      <el-button type="primary" @click="updateChart">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'chartProp',
+  data () {
+    return {
+      visible: false,
+      formLabelWidth: '100px',
+      form: {},
+      rules: {
+        catgory: [
+          { required: true, message: '请选择图表类别', trigger: 'blur' }
+        ],
+        value: [
+          { required: true, message: '请选择图表数据', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  computed: mapGetters({
+    fieldOptions: 'fieldOptions'
+  }),
+  methods: {
+    open (catgory, values) {
+      this.$refs.form && this.$refs.form.resetFields()
+      this.form = {
+        catgory: catgory,
+        value: [...values]
+      }
+      this.visible = true
+    },
+    updateChart () {
+      this.$emit('update', JSON.parse(JSON.stringify(this.form)))
+      this.visible = false
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
